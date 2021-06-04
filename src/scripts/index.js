87c8e60c27d75411b57438e3e62fae20020f3c49
mnooryusuf <mnyusuf14@gmail.com>
2021-06-04 10:43:02 +0800

    Menambahkan es lint dan mengubah font-awesome

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,39 +3,35 @@ import '../styles/main.css';
 import '../styles/responsive.css';
 import '../styles/skip-link.css';
 
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
 import data from '../DATA.json';
 
-const navToggler = document.querySelector(".nav-toggler");
-navToggler.addEventListener("click", toggleNav);
+const navToggler = document.querySelector('.nav-toggler');
+navToggler.addEventListener('click', toggleNav);
 
-const listRestaurantElement = document.querySelector(".restaurant");
+const listRestaurantElement = document.querySelector('.restaurant');
 listRestaurantElement.innerHTML = '';
 
-function toggleNav(){
-    navToggler.classList.toggle("active");
-    document.querySelector(".nav").classList.toggle("open");
+function toggleNav() {
+  navToggler.classList.toggle('active');
+  document.querySelector('.nav').classList.toggle('open');
 }
 
-document.addEventListener("click", function(e){
-    if(e.target.closest(".nav-item")){
-       toggleNav(); 
-    }
+document.addEventListener('click', (e) => {
+  if (e.target.closest('.nav-item')) {
+    toggleNav();
+  }
 });
 
-window.addEventListener("scroll",function(){
-    if(this.pageYOffset > 60){
-        this.document.querySelector(".header").classList.add("sticky");
-    }else{
-        this.document.querySelector(".header").classList.remove("sticky");
-    }
+window.addEventListener('scroll', function () {
+  if (this.pageYOffset > 60) {
+    this.document.querySelector('.header').classList.add('sticky');
+  } else {
+    this.document.querySelector('.header').classList.remove('sticky');
+  }
 });
 
 data.restaurants.forEach((restaurant) => {
-    listRestaurantElement.innerHTML += `
+  listRestaurantElement.innerHTML += `
     <div class="restaurant-items">
         <img tabindex="0" src="${restaurant.pictureId}" alt="${restaurant.name}">
         <span class="restaurant-address">
@@ -53,9 +49,3 @@ data.restaurants.forEach((restaurant) => {
     </div>
     `;
 });
-
-
-
-
-
-
