52cedac8cd9034acacd0fc7bda3ac63340619f20
mnooryusuf <mnyusuf14@gmail.com>
2021-07-03 11:20:03 +0800

    TODO Harus Dikerjakan

--- specs/likeRestaurantSpec.js
+++ specs/likeRestaurantSpec.js
@@ -67,7 +67,7 @@ describe('Liking A Restaurant', () => {
     FavoriteRestaurantIdb.deleteRestaurant(1);
   });
 
-  xit('should not add a restaurant when it has no id', async () => {
+  it('should not add a restaurant when it has no id', async () => {
     await LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       restaurant: {},
