97e62b1f8390218d28e6f72fcfad490c5fffc264
mnooryusuf <mnyusuf14@gmail.com>
2021-07-06 09:59:07 +0800

    Membangun Fitur Menggunakan TDD

--- src/scripts/data/favoriterestaurant-idb.js
+++ src/scripts/data/favoriterestaurant-idb.js
@@ -30,6 +30,10 @@ const FavoriteRestaurantIdb = {
   async deleteRestaurant(id) {
     return (await dbPromise).delete(OBJECT_STORE_NAME, id);
   },
+
+  async searchRestaurants(query) {
+
+  },
 };
 
 export default FavoriteRestaurantIdb;
