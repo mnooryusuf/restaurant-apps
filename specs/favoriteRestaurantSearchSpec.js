6baff0b0e92278e180e2047d0142b2792b237b55
mnooryusuf <mnyusuf14@gmail.com>
2021-07-08 14:54:57 +0800

    Jangan Bertele-tele

--- specs/favoriteRestaurantSearchSpec.js
+++ specs/favoriteRestaurantSearchSpec.js
@@ -2,42 +2,78 @@ import FavoriteRestaurantSearchPresenter from '../src/scripts/views/pages/liked-
 import FavoriteRestaurantIdb from '../src/scripts/data/favoriterestaurant-idb';
 
 describe('Searching restaurants', () => {
-  beforeEach(() => {
+  let presenter;
+
+  const searchRestaurants = (query) => {
+    const queryElement = document.getElementById('query');
+    queryElement.value = query;
+    queryElement.dispatchEvent(new Event('change'));
+  };
+
+  const setRestaurantSearchContainer = () => {
     document.body.innerHTML = `
-            <div id="restaurant-search-container">
-                <input id="query" type="text">
-                <div class="restaurant-result-container">
-                    <ul class="restaurants">
-                    </ul>
-                </div>
+        <div id="restaurant-search-container">
+            <input id="query" type="text">
+            <div class="restaurant-result-container">
+                <ul class="restaurants">
+                </ul>
             </div>
-            `;
+        </div>
+        `;
+  };
+
+  const constructPresenter = () => {
+    spyOn(FavoriteRestaurantIdb, 'searchRestaurants');
+    presenter = new FavoriteRestaurantSearchPresenter({
+      favoriteRestaurants: FavoriteRestaurantIdb,
+    });
+  };
+
+  beforeEach(() => {
+    setRestaurantSearchContainer();
+    constructPresenter();
   });
 
   it('should be able to capture the query typed by the user', () => {
-    spyOn(FavoriteRestaurantIdb, 'searchRestaurants');
-    const presenter = new FavoriteRestaurantSearchPresenter({
-      favoriteRestaurants: FavoriteRestaurantIdb,
-    });
-
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'restaurant a';
-    queryElement.dispatchEvent(new Event('change'));
+    searchRestaurants('restaurant a');
 
     expect(presenter.latestQuery).toEqual('restaurant a');
   });
 
   it('should ask the model to search for liked restaurants', () => {
-    spyOn(FavoriteRestaurantIdb, 'searchRestaurants');
-    const presenter = new FavoriteRestaurantSearchPresenter({
-      favoriteRestaurants: FavoriteRestaurantIdb,
-    });
+    searchRestaurants('restaurant a');
 
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'restaurant a';
-    queryElement.dispatchEvent(new Event('change'));
+    expect(FavoriteRestaurantIdb.searchRestaurants).toHaveBeenCalledWith(
+      'restaurant a',
+    );
+  });
 
-    expect(FavoriteRestaurantIdb.searchRestaurants)
-      .toHaveBeenCalledWith('restaurant a');
+  it('should show the found restaurants', () => {
+    presenter._showFoundRestaurants([{ id: 1, title: 'Restaurant Satu' }]);
+    expect(document.querySelectorAll('.restaurant').length).toEqual(1);
+
+    presenter._showFoundRestaurants([{ id: 1, title: 'Satu' }, { id: 2, title: 'Dua' }]);
+    expect(document.querySelectorAll('.restaurant').length).toEqual(2);
+  });
+
+  it('should show the title of the found restaurants', () => {
+    presenter._showFoundRestaurants([{ id: 1, title: 'Satu' }]);
+    expect(document.querySelectorAll('.restaurant__title').item(0).textContent)
+      .toEqual('Satu');
+
+    presenter._showFoundRestaurants(
+      [{ id: 1, title: 'Satu' }, { id: 2, title: 'Dua' }],
+    );
+
+    const restaurantTitles = document.querySelectorAll('.restaurant__title');
+    expect(restaurantTitles.item(0).textContent).toEqual('Satu');
+    expect(restaurantTitles.item(1).textContent).toEqual('Dua');
+  });
+
+  it('should show - for found restaurant without title', () => {
+    presenter._showFoundRestaurants([{ id: 1 }]);
+
+    expect(document.querySelectorAll('.restaurant__title').item(0).textContent)
+      .toEqual('-');
   });
 });
--- src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-presenter.js
+++ src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-presenter.js
@@ -7,9 +7,22 @@ class FavoriteRestaurantSearchPresenter {
   _listenToSearchRequestByUser() {
     this._queryElement = document.getElementById('query');
     this._queryElement.addEventListener('change', (event) => {
-      this._latestQuery = event.target.value;
-      this._favoriteRestaurants.searchRestaurants(this._latestQuery);
+      this._searchRestaurants(event.target.value);
     });
+  }
+
+  _searchRestaurants(latestQuery) {
+    this._latestQuery = latestQuery;
+    this._favoriteRestaurants.searchRestaurants(this.latestQuery);
+  }
+
+  _showFoundRestaurants(restaurants) {
+    const html = restaurants.reduce(
+      (carry, restaurant) => carry.concat(`<li class="restaurant"><span class="restaurant__title">${restaurant.title || '-'}</span></li>`),
+      '',
+    );
+
+    document.querySelector('.restaurants').innerHTML = html;
   }
 
   get latestQuery() {
