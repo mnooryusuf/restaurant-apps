44ebb7f98f965fce2f3c2aef020e8fb361f99937
mnooryusuf <mnyusuf14@gmail.com>
2021-07-09 10:23:06 +0800

    Kau Tak Perlu Tahu Rahasiaku

--- specs/favoriteRestaurantSearchSpec.js
+++ specs/favoriteRestaurantSearchSpec.js
@@ -46,35 +46,6 @@ describe('Searching restaurants', () => {
       );
     });
 
-    it('should show the found restaurants', () => {
-      presenter._showFoundRestaurants([{ id: 1, title: 'Restaurant Satu' }]);
-      expect(document.querySelectorAll('.restaurant').length).toEqual(1);
-
-      presenter._showFoundRestaurants([{ id: 1, title: 'Satu' }, { id: 2, title: 'Dua' }]);
-      expect(document.querySelectorAll('.restaurant').length).toEqual(2);
-    });
-
-    it('should show the title of the found restaurants', () => {
-      presenter._showFoundRestaurants([{ id: 1, title: 'Satu' }]);
-      expect(document.querySelectorAll('.restaurant__title').item(0).textContent)
-        .toEqual('Satu');
-
-      presenter._showFoundRestaurants(
-        [{ id: 1, title: 'Satu' }, { id: 2, title: 'Dua' }],
-      );
-
-      const restaurantTitles = document.querySelectorAll('.restaurant__title');
-      expect(restaurantTitles.item(0).textContent).toEqual('Satu');
-      expect(restaurantTitles.item(1).textContent).toEqual('Dua');
-    });
-
-    it('should show - for found restaurant without title', () => {
-      presenter._showFoundRestaurants([{ id: 1 }]);
-
-      expect(document.querySelectorAll('.restaurant__title').item(0).textContent)
-        .toEqual('-');
-    });
-
     it('should show the restaurants found by Favorite Restaurants', (done) => {
       document.getElementById('restaurant-search-container')
         .addEventListener('restaurants:searched:updated', () => {
@@ -105,6 +76,21 @@ describe('Searching restaurants', () => {
         { id: 111, title: 'restaurant abc' },
         { id: 222, title: 'ada juga restaurant abcde' },
         { id: 333, title: 'ini juga boleh restaurant a' },
+      ]);
+
+      searchRestaurants('restaurant a');
+    });
+
+    it('should show - when the restaurant returned does not contain a title', (done) => {
+      document.getElementById('restaurant-search-container').addEventListener('restaurants:searched:updated', () => {
+        const restaurantTitles = document.querySelectorAll('.restaurant__title');
+        expect(restaurantTitles.item(0).textContent).toEqual('-');
+
+        done();
+      });
+
+      favoriteRestaurants.searchRestaurants.withArgs('restaurant a').and.returnValues([
+        { id: 444 },
       ]);
 
       searchRestaurants('restaurant a');
