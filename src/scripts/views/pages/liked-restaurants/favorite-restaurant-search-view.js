19c893dc62dc98818f5dd1ca512da1990986b5d0
mnooryusuf <mnyusuf14@gmail.com>
2021-07-10 15:13:44 +0800

    Plot Twist

--- src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-view.js
+++ src/scripts/views/pages/liked-restaurants/favorite-restaurant-search-view.js
@@ -1,3 +1,5 @@
+import { createRestaurantItemTemplate } from '../../templates/template-creator';
+
 class FavoriteRestaurantSearchView {
   getTemplate() {
     return `
@@ -9,6 +11,24 @@ class FavoriteRestaurantSearchView {
                   </div>
               </div>
               `;
+  }
+
+  getFavoriteRestaurantTemplate() {
+    return `
+    <section class="explore-section sec-padding" id="explore">
+          <div class="container">
+              <div class="row">
+                  <div class="section-title" >
+                      <h2 id="maincontent" tabindex="0" data-title="explore">Favorite Restaurant</h2>
+                  </div>
+              </div>
+              <div class="row ">
+                  <div id="restaurants" class="restaurant">
+                  </div>
+              </div>               
+          </div>
+      </section>       
+       `;
   }
 
   runWhenUserIsSearching(callback) {
@@ -33,6 +53,18 @@ class FavoriteRestaurantSearchView {
 
     document.getElementById('restaurant-search-container')
       .dispatchEvent(new Event('restaurants:searched:updated'));
+  }
+
+  showFavoriteRestaurants(restaurants = []) {
+    let html;
+    if (restaurants.length) {
+      html = restaurants.reduce((carry, restaurant) => carry.concat(createRestaurantItemTemplate(restaurant)), '');
+    } else {
+      html = '<div class="restaurant-item__not__found"></div>';
+    }
+    document.getElementById('restaurants').innerHTML = html;
+
+    document.getElementById('restaurants').dispatchEvent(new Event('restaurants:updated'));
   }
 }
 
