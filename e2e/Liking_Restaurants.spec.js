9f7b9f72dcfa907a123d280961f596ae8e21fec6
mnooryusuf <mnyusuf14@gmail.com>
2021-07-12 14:16:42 +0800

    Jangan Sampai Salah Nama

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
@@ -9,16 +11,23 @@ Scenario('showing empty liked restaurants', ({ I }) => {
   I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
 });
 
-Scenario('liking one restaurant', ({ I }) => {
+Scenario('liking one restaurant', async ({ I }) => {
   I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
 
   I.amOnPage('/');
+
   I.seeElement('.restaurant__name a');
-  I.click(locate('.restaurant__name a').first());
+  const firstRestaurant = locate('.restaurant__name a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/favorite');
   I.seeElement('.restaurant-items');
+
+  const likedRestaurantTitle = await I.grabTextFrom('.restaurant__name');
+
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
 });
