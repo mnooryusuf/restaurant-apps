4036cbfe41af5ec62d7fedc8a3e72e8bbef62da3
mnooryusuf <mnyusuf14@gmail.com>
2022-04-25 02:51:12 +0800

    Fix Post Review

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,9 +1,8 @@
 const CONFIG = {
-  KEY: '12345',
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 1,
   DATABASE_NAME: 'restaurant-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
